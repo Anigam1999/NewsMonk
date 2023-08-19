0dfce9797c596987610b40c1f3d8943b65926d93
Abhishek Nigam <abhisheknigam755@gmail.com>
2023-08-20 01:09:52 +0530

    newsApp by class component

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,12 @@ export class NewsItem extends React.Component {
     return (
         <div className='my-3'>
             <div className="card">
-            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>     {source}</span>
+              <div style={{display: 'flex',
+                justifyContent: 'flex-end',
+                position: 'absolute',
+                right: '0'}}>
+                <span className="badge rounded-pill bg-danger">{source}</span>
+              </div>
             <img src={imageUrl?imageUrl:"https://cdn.pixabay.com/photo/2015/02/15/09/33/news-636978_1280.jpg"} className="card-img-top" alt="..." />
             <div className="card-body">
                 <h5 className="card-title">{title}
